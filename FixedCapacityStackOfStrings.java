080df31af011a6d349fe327852137baf9c47312d
Pulkit Mittal <46241207+Pulkit3108@users.noreply.github.com>
2020-01-22 22:13:51 +0530

    Update FixedCapacityStackOfStrings.java

--- FixedCapacityStackOfStrings.java
+++ FixedCapacityStackOfStrings.java
@@ -12,7 +12,9 @@ public class FixedCapacityStackOfStrings
         s[N++]=item;
     }
     public String pop(){
-        return(s[--N]);
+	String item = s[--N];
+	s[N] = null;    
+        return(item);
     }
 
 	public static void main(String[] args) {
